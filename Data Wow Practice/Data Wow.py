34c4d5f7f5b4f25099dc15f776b796a7d893a03a
Odiosaser <supitchaklanpradit@hotmail.com>
2020-09-14 17:49:47 +0700

    Update Data Wow.py

--- Data Wow Practice/Data Wow.py
+++ Data Wow Practice/Data Wow.py
@@ -2,20 +2,17 @@ import math
 
 def solution(indices, K):
     answer = []
-    all_fold = []
     num_ele_fold = math.ceil(len(indices)/K)
 
     mini_fold = []
     for index,element in enumerate(indices):
         mini_fold.append(element)
         if index % num_ele_fold == num_ele_fold-1 and index !=0:
-            all_fold.append(mini_fold)
             not_mini_fold = [train for index_train, train in enumerate(indices) if train not in mini_fold]
             answer.append(mini_fold)
             answer.append(not_mini_fold)
             mini_fold = []
         elif index == len(indices)-1:
-            all_fold.append(mini_fold)
             not_mini_fold = [train for index_train, train in enumerate(indices) if train not in mini_fold]
             answer.append(mini_fold)
             answer.append(not_mini_fold)
